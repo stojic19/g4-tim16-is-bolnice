22cc7ecde34aa8f60ecb172f304571ec66cbbce0
Magdalena Reljin <reljin.ra82.2018@uns.ac.rs>
2021-06-11 18:38:24 +0200

    #pacijent feedback

---
+++ BOLNICA/BOLNICA/View/PacijentView/PacijentGlavniProzor.xaml.cs
@@ -0,0 +1,116 @@
+﻿using Bolnica.Kontroler;
+using Bolnica.PacijentFolder;
+using Bolnica.Repozitorijum;
+using Bolnica.View.PacijentFolder;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bolnica
+{
+    public partial class PacijentGlavniProzor : Window
+    {
+        private static Grid GlavniSadrzaj;
+        private String korisnickoIme;
+        public PacijentGlavniProzor(String korisnickoIme)
+        {
+            InitializeComponent();
+            this.korisnickoIme = korisnickoIme;
+            GlavniSadrzaj = this.MainPanel;
+            PromeniPrikaz(new PrikazObavestenjaPacijent(korisnickoIme));
+            SetNaslov("Obaveštenja");
+        }
+        
+        public static Grid GetGlavniSadrzaj()
+        {
+            return GlavniSadrzaj;
+        }
+
+        public void PromeniPrikaz(UserControl userControl)
+        {
+            MainPanel.Children.Clear();
+            MainPanel.Children.Add(userControl);
+        }
+        public void SetNaslov(String naslov)
+        {
+            naslovStrane.Content = "ZdravoKlinika -  " + naslov;
+        }
+
+        private void Obavestenja_Click(object sender, RoutedEventArgs e)
+        {
+            PromeniPrikaz(new PrikazObavestenjaPacijent(korisnickoIme));
+            SetNaslov("Obaveštenja");
+        }
+
+        private void Zakazi_Click(object sender, RoutedEventArgs e)
+        {
+            PromeniPrikaz(new ZakazivanjeSaPrioritetomPacijent(korisnickoIme));
+            SetNaslov("Zakaži pregled");
+        }
+
+        private void Raspored_Click(object sender, RoutedEventArgs e)
+        {
+            PromeniPrikaz(new PrikazRasporedaPacijent(korisnickoIme));
+            SetNaslov("Raspored");
+        }
+
+        private void Terapija_Click(object sender, RoutedEventArgs e)
+        {
+            PromeniPrikaz(new PrikazTerapijePacijent(korisnickoIme));
+            SetNaslov("Terapija");
+        }
+
+        private void Karton_Click(object sender, RoutedEventArgs e)
+        {
+            PromeniPrikaz(new PrikazKartona(korisnickoIme));
+            SetNaslov("Zdravstvenii karton");
+        }
+
+        private void Ankete_Click(object sender, RoutedEventArgs e)
+        {
+            PromeniPrikaz(new PrikazAnketa(korisnickoIme));
+            SetNaslov("Ankete");
+        }
+
+        private void Pomoc_Click(object sender, RoutedEventArgs e)
+        {
+            PromeniPrikaz(new Pomoc());
+            SetNaslov("Pomoć");
+        }
+
+        private void MojNalog_Click(object sender, RoutedEventArgs e)
+        {
+            PromeniPrikaz(new LicniProfil(korisnickoIme));
+            SetNaslov("Moj nalog");
+        }
+
+        private void OdjaviSe_Click(object sender, RoutedEventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            this.Close();
+        }
+
+        private void Oceni_Click(object sender, RoutedEventArgs e)
+        {
+            PromeniPrikaz(new OceniAplikaciju(korisnickoIme));
+            SetNaslov("Prijavi problem");
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+    }
+}
--- BOLNICA/BOLNICA/ViewModel/PacijentViewModel/OcenaAplikacijeViewModel.cs
+++ BOLNICA/BOLNICA/ViewModel/PacijentViewModel/OcenaAplikacijeViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Bolnica.Komande;
+using Bolnica.Kontroler;
+using Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,65 +11,112 @@ namespace Bolnica.ViewModel.PacijentViewModel
 {
     public class OcenaAplikacijeViewModel: ViewModel
     {
-       // private OcenaAplikacijeDTO ocenaAplikacije = new OcenaAplikacijeDTO();
-        private String korisnickoIme;
+        private FeedbackKontroler feedbackKontroler = new FeedbackKontroler();
         private String poruka;
-       // private OcenaAplikacijeKontroler ocenaAplikacijeKontroler = new OcenaAplikacijeKontroler();
-
+        private FeedbackDTO podaci;
+        private List<String> tipovi;
+        private List<String> funkcionalnosti;
+        private String korisnickoIme;
         public OcenaAplikacijeViewModel(String korisnickoIme)
         {
+            Podaci = new FeedbackDTO();
             this.korisnickoIme = korisnickoIme;
-            sacuvajOcenu = new RelayCommand(Sacuvaj);
+            UcitajTipove();
+            UcitajFunkcionalnosti();
+            potvrdiKomanda = new RelayCommand(Potvrdi);
         }
-        private int ocenaCombo;
-        public int OcenaCombo
+
+        private void UcitajTipove()
         {
-            get { return ocenaCombo; }
-            set
-            {
-                ocenaCombo = value;
-                OnPropertyChanged();
-            }
+            Tipovi = new List<String>();
+            Tipovi.Add("Izgled aplikacije");
+            Tipovi.Add("Rad aplikacije");
+            Tipovi.Sort();
         }
-        public String Poruka
+
+        private void UcitajFunkcionalnosti()
         {
-            get { return poruka; }
-            set
-            {
-                poruka = value;
-                OnPropertyChanged();
-            }
+            Funkcionalnosti = new List<String>();
+            Funkcionalnosti.Add("Lični profil");
+            Funkcionalnosti.Add("Lična obaveštenja");
+            Funkcionalnosti.Add("Zakazivanje termina");
+            Funkcionalnosti.Add("Pomeranje termina");
+            Funkcionalnosti.Add("Otkazivanje termina");
+            Funkcionalnosti.Add("Terapija");
+            Funkcionalnosti.Add("Beleške");
+            Funkcionalnosti.Add("Podestnik");
+            Funkcionalnosti.Add("Izveštaj");
+            Funkcionalnosti.Add("Ankete");
+            Funkcionalnosti.Add("Pomoć");
         }
-      /*  public OcenaAplikacijeDTO OcenaAplikacije
-        {
-            get { return ocenaAplikacije; }
-            set
-            {
-                ocenaAplikacije = value;
-                OnPropertyChanged();
-            }
-        }*/
         public String KorisnickoIme
         {
             get { return korisnickoIme; }
-
-        }
-        private RelayCommand sacuvajOcenu;
-        public RelayCommand SacuvajOcenu
-        {
-            get { return sacuvajOcenu; }
-        }
-        public void Sacuvaj()
-        {
-            int ocena = OcenaCombo + 1;
-            if (ocena != 0)
+            set
             {
-               /* ocenaAplikacije.KorisnickoIme = KorisnickoIme;
-                ocenaAplikacije.Datum = DateTime.Now;
-                ocenaAplikacije.Ocena = ocena;
-                ocenaAplikacijeKontroler.SacuvajOcenuAplikacije(ocenaAplikacije);*/
-                Poruka = "Ocena sačuvana!";
+                korisnickoIme = value;
+                OnPropertyChanged();
             }
+        }
+
+        public List<String> Funkcionalnosti
+        {
+            get { return funkcionalnosti; }
+            set
+            {
+                funkcionalnosti = value;
+                OnPropertyChanged();
+            }
+        }
+        public List<String> Tipovi
+        {
+            get { return tipovi; }
+            set
+            {
+                tipovi = value;
+                OnPropertyChanged();
+            }
+        }
+        public FeedbackDTO Podaci
+        {
+            get { return podaci; }
+            set
+            {
+                podaci = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Poruka
+        {
+            get { return poruka; }
+            set { poruka = value; OnPropertyChanged("Poruka"); }
+        }
+
+        private RelayCommand potvrdiKomanda;
+
+        public RelayCommand PotvrdiKomanda
+        {
+            get { return potvrdiKomanda; }
+        }
+        public void Potvrdi()
+        {
+            if (IspravniUnetiPodaci())
+            {
+                Podaci.IdFeedbacka = Guid.NewGuid().ToString();
+                Podaci.IdOsobe = korisnickoIme;
+                feedbackKontroler.Dodaj(Podaci);
+                Poruka = "Problem uspešno prijavljen. Hvala na pomoći!";
+
+            }
+        }
+        private bool IspravniUnetiPodaci()
+        {
+            if (Podaci.TekstFeedbacka.Equals(""))
+            {
+                Poruka = "Morate uneti tekst feedback-a!";
+                return false;
+            }
+            return true;
         }
     }
 }
